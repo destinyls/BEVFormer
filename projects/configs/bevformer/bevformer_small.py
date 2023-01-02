a94e95ed22287e1b782b7b68836061dee0115848
destinyls <1491078277@qq.com>
2023-01-02 15:19:20 +0800

    large num_workers

--- projects/configs/bevformer/bevformer_small.py
+++ projects/configs/bevformer/bevformer_small.py
@@ -218,7 +218,7 @@ test_pipeline = [
 
 data = dict(
     samples_per_gpu=2,
-    workers_per_gpu=2,
+    workers_per_gpu=16,
     train=dict(
         type=dataset_type,
         data_root=data_root,
